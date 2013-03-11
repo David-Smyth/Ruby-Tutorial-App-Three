76e44ae94a61272f722fa13c38bffa4415c4625e
David E. Smyth, Chief Scientist <David.Smyth@Webair.com>
2013-03-11 11:45:22 -0700

    Initial commit of third app using RSpec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirdApp::Application.config.secret_token = '59c06368e1e3bb34ea9fa1801d5ac7fea93d92b64085231088cb04f53d53b56bee749788de4c4b63f726d2360da1d6a88607c66954791ecf0c3875c98556975b'
