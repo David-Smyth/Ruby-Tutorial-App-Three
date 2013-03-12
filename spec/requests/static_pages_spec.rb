2dd7e4f78c96c549a7c043b0e9f180862a061074
David E. Smyth, Chief Scientist <David.Smyth@Webair.com>
2013-03-12 16:05:43 -0700

    Complete exercise 2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
+  let(:base_title) { "Ruby on Rails Tutorial Third App" }
+  
   describe "Home page" do
     it "should have the title including 'Third App | Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => 'Third App | Home' )
+      page.should have_selector('title', :text => "#{base_title} | Home" )
     end
     it "should have heading including 'Third App'" do
       visit '/static_pages/home'
@@ -14,7 +17,7 @@ describe "StaticPages" do
   describe "Help page" do
     it "should have the title including 'Third App | Help'" do
       visit "/static_pages/help"
-      page.should have_selector( 'title', :text => 'Third App | Help')
+      page.should have_selector( 'title', :text => "#{base_title} | Help")
     end
     it "should have heading including 'Help'" do
       visit "/static_pages/help"
@@ -24,7 +27,7 @@ describe "StaticPages" do
   describe "About page" do
     it "should have the title including 'Third App | About'" do
       visit "/static_pages/about"
-      page.should have_selector( 'title', :text => 'Third App | About')
+      page.should have_selector( 'title', :text => "#{base_title} | About")
     end
     it "should have heading including 'About Us'" do
       visit "/static_pages/about"
@@ -34,7 +37,7 @@ describe "StaticPages" do
   describe "Contact page" do
     it "should have a title including 'Third App | Contact'" do
       visit "/static_pages/contact"
-      page.should have_selector( 'title', :text => 'Third App | Contact')
+      page.should have_selector( 'title', :text => "#{base_title} | Contact")
     end
     it "should have a heading including 'Contact'" do
       visit "/static_pages/contact"
